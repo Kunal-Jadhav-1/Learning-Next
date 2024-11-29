97c63ce154d5b4e04c76b1f47cc1f794fff15990
Kunal Jadhav <94116481+Kunal-Jadhav-1@users.noreply.github.com>
2024-11-30 04:55:00 +0530

    second commit

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,6 +1,6 @@
-export default async function Product({params}:{params:{id:string}}){
-    const {id} = await params;
-    return(
+export default async function Product({ params }: { params: { id: string } }) {
+    const { id } = params; 
+    return (
         <div>Product {id}</div>
-    )
+    );
 }
